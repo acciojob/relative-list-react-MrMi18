a50db0f074b3a29f70fa8408a03c8047ba29bb5f
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-05-31 17:14:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const App = () => {
   const[list , setList] = useState(["a","b","c"]);
  return (
-    <ol key={relativeList}>
+    <ol key={"relativeList"}>
       {
         list.map((r,index) => {
           return <li key={"relativeListItem1"+index}>{r}</li>
